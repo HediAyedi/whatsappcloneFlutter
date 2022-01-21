92c2ffda4b80a2262b8c2af44984bcbcc50b1b53
Clan1Rex <hediayedi20@gmail.com>
2022-01-21 11:32:30 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:whatsappclone/colors.dart';
+import 'package:whatsappclone/responsive/responsive_layout.dart';
+import 'package:whatsappclone/screens/mobile_sreen_layout.dart';
+import 'package:whatsappclone/screens/web_screen_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'What\'s App',
+      theme: ThemeData.dark().copyWith(
+        backgroundColor: backgroundColor,
+      ),
+      home: ResponsiveLayout(mobileScreenLayout:  MobileScreenLayout(), webScreenLayout:  WebScreenLayout()),
+    );
+  }
+}
